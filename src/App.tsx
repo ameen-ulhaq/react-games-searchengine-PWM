2d7984f1abae2a80e6c937c5c0bb46ec3980faf4
ameen-ulhaq <dev.ameenulhaq@gmail.com>
2023-05-06 18:17:35 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import Alert from "./componenets/Alert";
+import Button from "./componenets/Button";
 import ListGroup from "./componenets/ListGroup";
 
 function App() {
   let items = ["New York", "San Fransico", "Tokoyo", "London", "Paris"];
+
+  const [alertvisible, setalertvisiblility] = useState(false);
 
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
   return (
     <div>
+      {alertvisible && (
+        <Alert onClose={() => setalertvisiblility(false)}>Hello World</Alert>
+      )}
+
+      <Button onClick={() => setalertvisiblility(true)}>My Btn</Button>
       <ListGroup
         items={items}
         heading="Cities"
